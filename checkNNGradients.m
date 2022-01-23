65f66107bd3ed8a214eda32e98bbc8e7ecb97026
arjuns238 <92126319+arjuns238@users.noreply.github.com>
2022-01-23 01:36:39 -0500

    Update checkNNGradients.m

--- checkNNGradients.m
+++ checkNNGradients.m
@@ -1,6 +1,5 @@
 function checkNNGradients(lambda)
-%CHECKNNGRADIENTS Creates a small neural network to check the
-%backpropagation gradients
+
 %   CHECKNNGRADIENTS(lambda) Creates a small neural network to check the
 %   backpropagation gradients, it will output the analytical gradients
 %   produced by your backprop code and the numerical gradients (computed
@@ -37,16 +36,15 @@ numgrad = computeNumericalGradient(costFunc, nn_params);
 % Visually examine the two gradient computations.  The two columns
 % you get should be very similar. 
 disp([numgrad grad]);
-fprintf(['The above two columns you get should be very similar.\n' ...
-         '(Left-Your Numerical Gradient, Right-Analytical Gradient)\n\n']);
+fprintf(['The above two columns should be very similar.\n' ...
+         '(Left-Numerical Gradient, Right-Analytical Gradient)\n\n']);
 
 % Evaluate the norm of the difference between two solutions.  
 % If you have a correct implementation, and assuming you used EPSILON = 0.0001 
 % in computeNumericalGradient.m, then diff below should be less than 1e-9
 diff = norm(numgrad-grad)/norm(numgrad+grad);
 
-fprintf(['If your backpropagation implementation is correct, then \n' ...
-         'the relative difference will be small (less than 1e-9). \n' ...
+fprintf(['the relative difference will be small (less than 1e-9). \n' ...
          '\nRelative Difference: %g\n'], diff);
 
 end
